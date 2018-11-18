9ccfc12d235e4936a8c00cd0bccda7ef57129fa9
alineregioli <alineregioli@hotmail.com>
2018-11-18 19:44:44 -0200

    detalhes

--- view/wa.php
+++ view/wa.php
@@ -6,14 +6,14 @@
 		<div class="container" style="background:white; margin-top: 10px;">
 			<div class="row" >
 				<div class="col-md-12 centro text-center" style="margin-top: 10px;">
-			<h1>Você acertou!</h1>
-		<img src="img/acc.gif" class="rounded-circle" height="400px" alt="Responsive image">
+			<h1>Você errou, tente novamente!</h1>
+		<img src="img/wa.gif" class="img-fluid rounded-circle" alt="Responsive image">
 					
 				</div>
 			</div>
 		</div>
 		<!-- rodapé -->
-		 
+		
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
 		<script src="bootstrap/js/bootstrap.min.js"></script>
